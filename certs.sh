2869da6050a85ccca2e9596cc9d289962daaee72
Mike Jones <mike.jones@manchester.ac.uk>
2026-06-18 15:04:21 +0000

    Allow env, args and wildcards

--- certs.sh
+++ certs.sh
@@ -3,13 +3,19 @@
 # Script to Create a fake CA, issue certs for servers as specified in servers array with Domain: DOMAIN.
 # Then secure copy each of the server's pfx, crt, and key files, and CAcert to REMOTEUSER@server.DOMAIN:UPLOADPATH
 #
-# Alter the following 5 declarations to suit:
+# Alter the following 5 declarations to suit: OR call this script with the env
 
-declare -a servers=("test1" "test2")
-DOMAIN=example.com
-CAName="Example-CA"
-UPLOADPATH="etc/"
-REMOTEUSER=someuser
+if [ "$1" ]
+then
+  declare -a servers=("$@")
+else
+  declare -a servers=("test1" "test2")
+fi
+DOMAIN=${DOMAIN:-example.org}
+CAName=${CAName:-Example-CA}
+UPLOADPATH=${UPLOADPATH:-etc/}
+REMOTEUSER=${REMOTEUSER:-$(id -un)}
+MAKEWILDCARD=${MAKEWILDCARD:-no}
 
 read -s -p "Set Password for the CA key: " CAPASSWD
 echo
@@ -60,8 +66,13 @@ echo " ! dig $fqdn +short |grep -q . && echo "Server $fqdn unresolvable " && con
 authorityKeyIdentifier=keyid,issuer
 basicConstraints=CA:FALSE
 keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
-subjectAltName = DNS:$fqdn
 EOF
+  if echo "$MAKEWILDCARD" | grep -qi "^y\(es\|\)$"
+  then 
+    echo "subjectAltName = DNS:$fqdn, DNS:*.$fqdn " >> dshubssl-ext.cnf
+  else
+    echo "subjectAltName = DNS:$fqdn" >> dshubssl-ext.cnf
+  fi
   openssl req -new -nodes -out $fqdn-${CAName}.csr -newkey rsa:4096 -keyout $fqdn-${CAName}.key -subj "/CN=$fqdn"
   openssl x509 -req -in $fqdn-${CAName}.csr -CA ${CAName}.crt -CAkey ${CAName}.key -CAcreateserial -out $fqdn-${CAName}.crt -days 4385 -sha256 -extfile dshubssl-ext.cnf
   openssl pkcs12 -export -out $fqdn-${CAName}.pfx -inkey $fqdn-${CAName}.key -in $fqdn-${CAName}.crt -certfile ${CAName}.crt -passout pass:
